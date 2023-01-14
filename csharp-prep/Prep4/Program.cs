595b31f7028c2f22622c0756e0bc7fee9146bffc
Kaushal Kafle <kaf17001@byui.edu>
2023-01-14 16:36:45 -0700

    Prep 4 completed!

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userInput = 1;
+
+        do 
+        {
+
+            Console.WriteLine("Enter a number: ");
+
+            string userAnswer = Console.ReadLine();
+            userInput = int.Parse(userAnswer);
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+
+        } 
+        while(userInput != 0);
+
+        int sum = 0;
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+            
+        }
+
     }
 }
