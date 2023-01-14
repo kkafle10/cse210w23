5f52dee94bb32178659826b53fe194ea1b6b7ebe
Kaushal Kafle <kaf17001@byui.edu>
2023-01-14 16:53:37 -0700

    Prep 5 completed!

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = userName();
+        int number = userNumber();
+        int square = squareNumber(number);
+
+        displayResult(name, square);
+
     }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string userName()
+    {
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int userNumber()
+    {
+        Console.WriteLine("What is your favorite number? ");
+        string num = Console.ReadLine();
+        int number = int.Parse(num);
+        return number;
+    }
+
+    static int squareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void displayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}.");
+    }
+    
 }
