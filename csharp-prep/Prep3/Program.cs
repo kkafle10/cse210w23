50bf0e09ffbb83f196d9b38a59d7271235ec0dc8
Kaushal Kafle <kaf17001@byui.edu>
2023-01-14 15:55:47 -0700

    Prep 3 completed!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random generator = new Random();
+        int magicNumber = generator.Next(1, 10);
+
+        bool keepPlaying = true;
+
+        
+        while(keepPlaying) {
+            Console.Write("Guess a number between 1 and 10: ");
+            string userInput = Console.ReadLine();
+            int userGuess = int.Parse(userInput);
+
+            if (magicNumber == userGuess) {
+                Console.WriteLine("Nice! You guessed it!");
+                keepPlaying = false;
+            }
+            else if(magicNumber > userGuess) {
+                Console.WriteLine("Higher");
+            }
+            else{
+                Console.WriteLine("Lower");
+            }
+        
+        }
+
     }
 }
